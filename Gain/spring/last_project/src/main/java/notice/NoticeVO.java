9d958afc633b5fb0a897bfc0bd0c999c99fff033
PARK GA IN <84880886+PARKGAIN@users.noreply.github.com>
2022-09-13 14:43:33 +0900

    java Notice 패키지와 NoticeVO 추가했습니다

---
+++ Gain/spring/last_project/src/main/java/notice/NoticeVO.java
@@ -0,0 +1,31 @@
+package notice;
+
+public class NoticeVO {
+	private String title,content,today;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getToday() {
+		return today;
+	}
+
+	public void setToday(String today) {
+		this.today = today;
+	}
+	
+	
+}
