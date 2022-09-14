3343ed51ba3753fa6f9abc6f27755506611b88e8
PARK GA IN <pgi082247@gmail.com>
2022-09-14 18:56:01 +0900

    안드로이드 공지사항 화면 익스펜더블 리사이클러뷰 추가 중입니다

---
+++ Gain/android/Last_Project/src/main/java/com/example/last_project/notice/NoticeActivity.java
@@ -0,0 +1,60 @@
+package com.example.last_project.notice;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.last_project.NoticeAdapter;
+import com.example.last_project.NoticeDTO;
+import com.example.last_project.R;
+import com.example.last_project.conn.CommonConn;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+
+public class NoticeActivity extends AppCompatActivity {
+    RecyclerView recv_notice;
+    ArrayList<NoticeDTO> list;
+    TextView title;
+    TextView content;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice);
+        recv_notice = findViewById(R.id.recv_notice);
+        title = findViewById(R.id.tv_notice_title);
+        content = findViewById(R.id.tv_notice_content);
+
+        CommonConn conn = new CommonConn(NoticeActivity.this, "notice");
+        conn.executeConn(new CommonConn.ConnCallback() {
+            @Override
+            public void onResult(boolean isResult, String data) {
+                if (isResult) {
+                    list = new Gson().fromJson(data, new TypeToken<ArrayList<NoticeDTO>>() {
+                    }.getType());
+
+                    NoticeAdapter adapter = new NoticeAdapter(getLayoutInflater(), list);
+                    RecyclerView.LayoutManager manager = new LinearLayoutManager(NoticeActivity.this, RecyclerView.VERTICAL, false);
+                    recv_notice.setAdapter(adapter);
+                    recv_notice.setLayoutManager(manager);
+
+
+
+                }
+            }
+        });
+
+        title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                content.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
