8466da9e88a037968cc14c60bbc319bb5bcb390d
PARK GA IN <pgi082247@gmail.com>
2022-09-14 15:07:13 +0900

    DB에 Notice 테이블에서 정보를 가져오기 위해 NoticeController 수정중입니다

--- Gain/spring/last_project/src/main/java/com/hanul/ateam/NoticeController.java
+++ Gain/spring/last_project/src/main/java/com/hanul/ateam/NoticeController.java
@@ -1,5 +1,16 @@
 package com.hanul.ateam;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import notice.NoticeDAO;
+
+@RestController
 public class NoticeController {
+	@Autowired private NoticeDAO dao;
+	
+	public void getList() {
+		dao.getList();
+	}
 
 }
--- Gain/spring/last_project/src/main/java/notice/NoticeDAO.java
+++ Gain/spring/last_project/src/main/java/notice/NoticeDAO.java
@@ -1,5 +1,7 @@
 package notice;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -8,5 +10,9 @@ import org.springframework.stereotype.Repository;
 public class NoticeDAO {
 	@Autowired private SqlSession sql;
 	
+	
+	public List<NoticeVO> getList() {
+		return sql.selectList("notice.select");
+	}
 	
 }
