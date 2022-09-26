a86a693c06ce46a33eb9ab673b39f48883725f93
ainH <i_taeja@naver.com>
2022-09-26 10:06:34 +0900

    삭제

--- Main/android/Last_Project/app/src/main/java/com/example/last_project/map/SameName.java
+++
@@ -1,9 +0,0 @@
-package com.example.last_project.map;
-
-import java.util.List;
-
-public class SameName {
-    private List<String> region;
-    private String keyword;
-    private String selected_region;
-}
