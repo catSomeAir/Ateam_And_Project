97e068860d32326be81bfaa49e0c61a84618ef4c
ainH <i_taeja@naver.com>
2022-09-30 14:17:51 +0900

    다시 커밋

---
+++ Main/android/Last_Project/app/src/main/java/com/example/last_project/main/market/Gift_Grid_List_Fragment.java
@@ -0,0 +1,25 @@
+package com.example.last_project.main.market;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.last_project.R;
+
+public class Gift_Grid_List_Fragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_gift__grid__list_, container, false);
+
+
+        return v;
+    }
+}
