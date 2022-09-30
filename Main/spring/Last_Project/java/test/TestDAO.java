fa2b8dc19ca5f312b47fe6e153bf323be7dabe4c
ainH <i_taeja@naver.com>
2022-09-30 10:18:08 +0900

    0930_spring

---
+++ Main/spring/Last_Project/java/test/TestDAO.java
@@ -0,0 +1,25 @@
+package test;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public class TestDAO implements TestService {
+	@Autowired private SqlSession sql;
+	@Override
+	public List<TestVO> test() {
+		
+		return sql.selectList("test.list");
+	}
+
+	@Override
+	public TestHotdogVO testhotdog(String email) {
+		return sql.selectOne("test.hotdog",email);
+	}
+
+}
